1ae0962790562032e7a7dfbb0fc04e8ed883a674
Vitalii Mikhailov <personal@aragas.org>
2025-12-05 22:24:46 +0200

    Fixed LauncherEx and Standalone crashes

--- src/Bannerlord.BLSE.Shared/Standalone.cs
+++ src/Bannerlord.BLSE.Shared/Standalone.cs
@@ -58,44 +58,61 @@ public static class Standalone
 
         if (hasModules || string.IsNullOrEmpty(saveFile))
             return;
-
-        // We need the initialization for Steam discovery to work
-        LauncherPlatform.Initialize();
-        Common.PlatformFileHelper = new PlatformFileHelperPC("Mount and Blade II Bannerlord");
+        
         try
         {
-            if (MBSaveLoad.GetSaveFileWithName(saveFile) is not { IsCorrupted: false } saveGameFileInfo) return;
-
-            var loadedModule = ModuleInfoHelper.GetModules();
-
-            var moduleNames = GetModules(saveGameFileInfo.MetaData).ToArray();
-            var existingModules = moduleNames.Select(x => loadedModule.FirstOrDefault(y => y.Name == x)).OfType<ModuleInfoExtended>().ToArray();
-            var existingModulesByName = existingModules.ToDictionary(x => x.Name, x => x);
-
-            var missingNames = moduleNames.Select(x => x).Except(existingModulesByName.Keys).ToArray();
-            if (missingNames.Length == 0)
+            // We need the initialization for Steam discovery to work
+            LauncherPlatformHelper.Initialize(args);
+            Common.PlatformFileHelper = new PlatformFileHelperPC("Mount and Blade II Bannerlord");
+            try
             {
-                args = args.Concat(new[] { $"_MODULES_*{string.Join("*", existingModules.Select(x => x.Id))}*_MODULES_" }).ToArray();
-                return;
-            }
+                if (MBSaveLoad.GetSaveFileWithName(saveFile) is not { IsCorrupted: false } saveGameFileInfo) return;
 
+                var loadedModule = ModuleInfoHelper.GetModules();
+
+                var moduleNames = GetModules(saveGameFileInfo.MetaData).ToArray();
+                var existingModules = moduleNames.Select(x => loadedModule.FirstOrDefault(y => y.Name == x)).OfType<ModuleInfoExtended>().ToArray();
+                var existingModulesByName = existingModules.ToDictionary(x => x.Name, x => x);
+
+                var missingNames = moduleNames.Select(x => x).Except(existingModulesByName.Keys).ToArray();
+                if (missingNames.Length == 0)
+                {
+                    args = args.Concat(new[] { $"_MODULES_*{string.Join("*", existingModules.Select(x => x.Id))}*_MODULES_" }).ToArray();
+                    return;
+                }
+
+                var message = $"""
+        No modules were provided as an argument!
+        Tried to use the load order from the save file {saveFile}, but it was missing the following mods:
+        {string.Join(Environment.NewLine, missingNames)}
+        Press Yes to exit, press No to continue loading
+        """;
+                switch (MessageBoxDialog.Show(message, "Error from BLSE!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1))
+                {
+                    case MessageBoxResult.Yes:
+                        Environment.Exit(1);
+                        return;
+                }
+            }
+            finally
+            {
+                LauncherPlatform.Destroy();
+                Common.PlatformFileHelper = null;
+            }
+        }
+        catch (Exception e)
+        {
             var message = $"""
-No modules were provided as an argument!
-Tried to use the load order from the save file {saveFile}, but it was missing the following mods:
-{string.Join(Environment.NewLine, missingNames)}
-Press Yes to exit, press No to continue loading
-""";
+                           Exception while attempting to load load order from save file:
+                           {e}
+                           Press Yes to exit, press No to continue loading
+                           """;
             switch (MessageBoxDialog.Show(message, "Error from BLSE!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1))
             {
                 case MessageBoxResult.Yes:
                     Environment.Exit(1);
                     return;
             }
-        }
-        finally
-        {
-            LauncherPlatform.Destroy();
-            Common.PlatformFileHelper = null;
         }
     }
 
