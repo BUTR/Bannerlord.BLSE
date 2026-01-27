0736712b06f580f0cfd68e3422f86582ed154f98
Vitalii Mikhailov <personal@aragas.org>
2026-01-27 13:49:35 +0200

    * Inject BLSE into vanilla launcher if Epic Games Store is detected to avoid workarounds
    * Fixed the decimal.Parse issue for certain locales

---
+++ src/Bannerlord.BLSE.Shared/AppDomainManager.cs
@@ -0,0 +1,36 @@
+﻿using Bannerlord.BLSE.Features.AssemblyResolver;
+using Bannerlord.BLSE.Features.Commands;
+using Bannerlord.BLSE.Features.ContinueSaveFile;
+using Bannerlord.BLSE.Features.Interceptor;
+using Bannerlord.BLSE.Features.Xbox;
+using Bannerlord.BLSE.Shared.Utils;
+
+using HarmonyLib;
+
+using System;
+using System.Runtime.InteropServices;
+
+using Windows.Win32;
+
+namespace Bannerlord.BLSE.Shared;
+
+public static class AppDomainManager
+{
+    private static readonly Harmony _featureHarmony = new("bannerlord.blse.features");
+  
+    public static void Initialize()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Environment.OSVersion.Version.Major >= 6)
+            PInvoke.SetProcessDPIAware();
+        
+        LauncherExceptionHandler.Watch();
+
+        InterceptorFeature.Enable(_featureHarmony);
+        AssemblyResolverFeature.Enable(_featureHarmony);
+        ContinueSaveFileFeature.Enable(_featureHarmony);
+        CommandsFeature.Enable(_featureHarmony);
+        XboxFeature.Enable(_featureHarmony);
+
+        ModuleInitializer.Disable();
+    }
+}
