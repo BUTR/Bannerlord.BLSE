57ff627183a02b75850e8237d3077696deffdf64
Vitalii Mikhailov <personal@aragas.org>
2026-05-16 14:52:46 +0300

    Increased stack reserve and commit according to vanilla Bannerlord
    (cherry picked from commit 9ab30744407406b823fb23a742738d2d22250b07)
    
    # Conflicts:
    #	changelog.txt

--- src/Bannerlord.LauncherEx/BUTRLauncherManagerHandler.cs
+++ src/Bannerlord.LauncherEx/BUTRLauncherManagerHandler.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection.Emit;
 
 using TaleWorlds.MountAndBlade.Launcher.Library;
 using TaleWorlds.MountAndBlade.Launcher.Library.UserDatas;
@@ -48,7 +49,18 @@ internal partial class BUTRLauncherManagerHandler : LauncherManagerHandler
 
         _harmony.Patch(
             AccessTools2.DeclaredPropertyGetter(typeof(LauncherUI), "AdditionalArgs"),
-            postfix: new HarmonyMethod(AccessTools2.DeclaredMethod(typeof(BUTRLauncherManagerHandler), nameof(AdditionalArgsPostfix)), priority: 10000));
+            postfix: new HarmonyMethod(AccessTools2.DeclaredMethod(typeof(BUTRLauncherManagerHandler), nameof(AdditionalArgsPostfix))));
+
+        // v1.4.x moved ModuleListCode into LauncherUI.AdditionalArgs, making the
+        // three-level forwarding chain (Program.StartGame -> StandaloneUIDomain.AdditionalArgs
+        // -> LauncherUI.AdditionalArgs)
+        _harmony.TryPatch(
+            AccessTools2.DeclaredPropertyGetter(typeof(StandaloneUIDomain), "AdditionalArgs"),
+            postfix: AccessTools2.DeclaredMethod(typeof(BUTRLauncherManagerHandler), nameof(AdditionalArgsPostfix)));
+
+        _harmony.TryPatch(
+            AccessTools2.DeclaredMethod(typeof(Program), "StartGame"),
+            transpiler: AccessTools2.DeclaredMethod(typeof(BUTRLauncherManagerHandler), nameof(BlankTranspiler)));
 
         // Note: the new LauncherManager removed loadOrderPersistenceProvider — load/save of the
         // user's saved order is now fully owned by BLSE. SaveTWLoadOrder is called directly from
@@ -83,6 +95,8 @@ internal partial class BUTRLauncherManagerHandler : LauncherManagerHandler
     {
         __result = Default._executableParameters;
     }
+
+    private static IEnumerable<CodeInstruction> BlankTranspiler(IEnumerable<CodeInstruction> instructions) => instructions;
 
     public void RegisterStateProvider(Func<LauncherState> getState)
     {
